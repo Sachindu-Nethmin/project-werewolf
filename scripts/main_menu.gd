13d8dda20d3c609e350dc49dc7eb3408e5d4040b
Sachindu-Nethmin <sachindunethminweerasinghe@gmail.com>
2026-04-12 00:10:26 +0530

    Adding day scene

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,10 +1,14 @@
 extends Control
 
 func _ready() -> void:
-	$VBox/PlayButton.pressed.connect(_on_play_pressed)
+	$VBox/DayButton.pressed.connect(_on_day_pressed)
+	$VBox/NightButton.pressed.connect(_on_night_pressed)
 	$VBox/QuitButton.pressed.connect(_on_quit_pressed)
 
-func _on_play_pressed() -> void:
+func _on_day_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_2.tscn")
+
+func _on_night_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
 
 func _on_quit_pressed() -> void:
